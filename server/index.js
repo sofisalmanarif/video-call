d97f7282f854293dfcd5c746e02eb11da12983a6
sofisalmanarif <sofisalman9906@gmail.com>
2024-09-01 14:26:38 +0530

    socket io and express setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log("first")
+import express from "express"
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app =express()
+
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors:true
+});
+
+io.on("connection", (socket) => {
+    console.log("user connected")
+  socket.on("join-room",({email,roomId})=>{
+    console.log(email,"user joined the room ",roomId)
+  })
+});
+
+
+
+io.listen(4000);
+app.listen(3000,()=>console.log("server listening"))
