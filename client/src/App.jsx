e288de2f7d4c31ceeb9caaa8ca34ab6653105f21
sofisalmanarif <sofisalman9906@gmail.com>
2024-09-01 14:11:03 +0530

    homepage and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div className="App bg-red-200">
-    hi
-   </div>
+ <>
+ <BrowserRouter>
+ <Routes>
+
+  <Route path="/" element={<Home/>}
+  
+  
+  />
+ </Routes>
+ 
+ </BrowserRouter>
+ </>
   )
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const Home = () => {
+    const [email, setemail] = useState("")
+    const [roomId, setRoomId] = useState("")
+
+    const submitHandler=()=>{
+        console.log(email,roomId)
+    }
+  return (
+    <div className='h-screen w-full flex items-center justify-center '>
+       <div className='flex gap-2 flex-col'>
+       <input value={email} onChange={(e)=>setemail(e.target.value)} className='rounded-md px-3 py-3 outline-none border-2 border-zinc-300' type="text" placeholder='Enter name'/>
+        <input value={roomId} onChange={(e)=>setRoomId(e.target.value)} className='rounded-md px-3 py-3 outline-none border-2 border-zinc-300' type="text"  placeholder='Enter Room id'/>
+        <button onClick={submitHandler} className='text-white bg-blue-500 hover:bg-blue-700 font-semibold text-md rounded-md px-20 py-3'> Join</button>
+       </div>
+    </div>
+  )
+}
+
+export default Home
